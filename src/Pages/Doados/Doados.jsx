403e05d1291b6739adf0247d2c806c0c26ae0372
lucziando <lucas.andrade9.la@gmail.com>
2025-04-03 23:28:11 -0300

    Integração da API

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,12 +1,36 @@
 import S from "./doados.module.scss"
 import revolucao from "../../assets/Imagens/revolucao.webp"
 
+import axios from "axios"
+import { useState, useEffect } from "react";
+
 export default function Doados(){
+
+    const [livros, setLivros] = useState([])
+
+    const getLivros = async () => {
+        const response = await axios.get("https://m2desafio2.onrender.com/livros")
+        setLivros(response.data)
+    }
+
+    useEffect(()=>{
+        getLivros();
+    },[])
     
    return(
     <section className={S.Doados}>
         <h2>Livros doados</h2>
         <section className={S.Cards}>
+
+        {livros.map((item)=>(
+                <article>
+                 <img className={S.Img} src={item.imagem_url} alt="" />
+                 <h3>{item.titulo}</h3>
+                  <p>{item.categoria}</p>
+                  <p>{item.autor}</p>
+                 </article>
+                ))}
+
             <article>
              <img className={S.Img} src={revolucao} alt="Capa do livro A revoluão Dos Bichos" />
 
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,22 +1,72 @@
 import S from "./querodoar.module.scss"
 import livro from '../../assets/Imagens/Vector.png'
 
+import axios from "axios"
+import { useState } from "react";
+
 export default function QueroDoar(){
+
+
+    const [titulo, setTitulo] = useState("")
+    const [categoria, setCategoria]= useState("")
+    const [autor, setAutor] = useState("")
+    const [imagem_url, setImagem_url] = useState("")
+
+    const enviarDados = async() => {
+
+        const urlApi = "https://m2desafio2.onrender.com/doar"
+
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            imagem_url,
+        }
+
+
+        await axios.post(urlApi,dadosEnviar)
+
+        alert("Livro Enviado")
+
+        setTitulo("")
+        setCategoria("")
+        setAutor("")
+        setImagem_url("")
+    }
+
+
+    const capturaTitulo = (e)=> {
+        setTitulo(e.target.value)
+        console.log(e.target.value)
+    }
+
+    const capturaCategoria = (e) => {
+        setCategoria(e.target.value)
+    }
+
+    const capturaAutor = (e) =>{
+        setAutor(e.target.value)
+    }
+
+    const capturaImagem = (e) => {
+        setImagem_url(e.target.value);
+    }
     return(
     //    <h2>EU QUERO DOAR</h2>
     <section className={S.Principal}>
         <section className={S.Conteiner}>
             <h2>Por favor, preencha o formulário com as suas informações e as informaçoões do livro</h2>
-            <form action="">
+            <form onSubmit={(e) => e.preventDefault()}>
                 <div>
                     <img src={livro} alt=" Ilustração de um livro" />
                     <h3>Informações do Livro</h3>
                 </div>
-                <input type="text" placeholder="Titulo" />
-                <input type="text" placeholder="Categoria" />
-                <input type="text" placeholder="Autor" />
-                <input type="text" placeholder="Link da Imagem" />
-                <input type="submit" value="Doar"/>
+                <input type="text" placeholder="Titulo" onChange={capturaTitulo} value={titulo}/>
+        <input type="text" placeholder="Categoria" onChange={capturaCategoria} value={categoria}/>
+        <input type="text" placeholder="Autor"
+        onChange={capturaAutor} value={autor}/>
+        <input type="text" placeholder="Link da Imagem" onChange={capturaImagem} value={imagem_url}/>
+        <input onClick={enviarDados} type="submit"  value="Doar"/>
             </form>
         </section>
     </section>
